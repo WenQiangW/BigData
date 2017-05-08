84c9f9df43aa0fbbc1ac41b00a3d62b0c55af1b2
WenQiang <WenQiangW@users.noreply.github.com>
2017-05-09 07:04:56 +0800

    Update BigData.hpp

--- BigData.hpp
+++ BigData.hpp
@@ -19,7 +19,7 @@ public:
 	BigData& operator=(const BigData& b);
 	friend ostream& operator<<(ostream& out, const BigData& bigdata);
 private:
-	bool IsINT64Overflow()const;//判断是否超过long
+	bool IsINT64Overflow()const;//判断是否超过long long
 
 	//Add  这些函数的参数 肯定都带符号，
 	string Add(string left, string right)
